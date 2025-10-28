11203328d51e84a9a9d0f30a2f2187e57d5ed73f
Your Name <you@example.com>
2025-10-28 11:34:39 +0530

    node and express

--- NodeJS/ExpressDemo.js
+++ NodeJS/ExpressDemo.js
@@ -1,23 +1,61 @@
 import data from "./data.json" with {type:'json'};
-// console.log(data);
 import express from "express"
+// console.log(data);
+
 // const express = require('express')
 // const data = require('./data.json')
-// const exp = require('express')
+
 const app = express()
 app.use(express.json())
+
+// app.get('/user/:id/:name',(req,res)=>{
 app.get('/user/:id',(req,res)=>{
+    // console.log(req.params.id,req.params.name);
     const userID = req.params.id;
     // console.log(userID);
     // res.send(userID);
-        const user = data.find(usr => usr.id === userID);
+        const user = data.find(usr => String(usr.id) === userID);
+        // console.log(user);
         if(user){
             res.json(user);
         }else{
             res.send('Error Finding User...')
         }
+        // console.log(res.send('Error Finding User...'))
 })
+
 app.get('/user',(req,res)=>{
-    res.json(data);
+    const pageno = parseInt(req.query.pageno);
+    if(pageno){
+        const recordLimit = 10;
+        // console.log(page-1);
+
+        const start = (pageno - 1) * recordLimit;
+        // console.log(start);
+
+        const end = start + recordLimit;
+        // console.log(end);
+    
+        const pagedData = data.slice(start, end);
+        res.json(pagedData);
+    }else{
+        res.json(data);
+    }
 })
+
+// app.get('/paging', (req, res) => {
+//     const pageno = parseInt(req.query.pageno) || 1; 
+//     const limit = 10; 
+//     // console.log(page-1);
+
+//     const start = (pageno - 1) * limit;
+//     // console.log(start);
+
+//     const end = start + limit;
+//     // console.log(end);
+
+//     const pagedData = data.slice(start, end);
+//     res.json(pagedData);
+// });
+
 app.listen(5000)
