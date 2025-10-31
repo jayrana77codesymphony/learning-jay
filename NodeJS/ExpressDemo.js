7bfb02881edd1030d1875bcbfa19a89e9ebac808
Your Name <you@example.com>
2025-10-31 12:27:42 +0530

    fix the conflict, and updated code

--- NodeJS/ExpressDemo.js
+++ NodeJS/ExpressDemo.js
@@ -1,91 +1,39 @@
-import data from "./data.json" with { type: 'json' }
-// console.log(data);
-
-import express from "express"
-
-// import fs from "fs"
-
-const app = express()
-// app.get('/user/:id/:name',(req,res)=>{})
-
-
-
-// --------------------------- Using es module -------------------------------
-
-app.get('/user/:id', (req, res) => {
-    const userID = req.params.id;
-    // console.log(userID);
-    // res.send(userID);
-    const user = data.find(usr => String(usr.id) === userID);
-    // console.log(user);
-    if (user) {
-        res.send(user);
-    } else {
-        res.send('Error Finding User...')
+import 'dotenv/config';
+import data from "./data.json" with { type: 'json' };
+import express from "express";
+const app = express();
+app.get('/users/:id', (req, res) => {
+    try {
+        const userID = Number(req.params.id);
+        const user = data.find(user => user.id === userID);
+        if (user) {
+            res.json({ status: 200, message: 'User found...', user: user });
+        } else {
+            res.json({ status: 404, error: 'User not found...' });
+        }
+    } catch (err) {
+        res.json({ status: 400, message: 'Error Fetching User Data...' });
     }
-    // console.log(res.send('Error Finding User...'))
-})
+});
 
-app.get('/user', (req, res) => {
-    const pageno = +req.query.pageno;
-    if (pageno) {
+app.get('/users', (req, res) => {
+    try {
+        const pageNumber = Number(req.query.pageNumber);
         const recordLimit = 10;
-        const start = (pageno - 1) * recordLimit;
-        const pagedData = data.slice(start, start + recordLimit);
-        res.send(pagedData);
-    } else {
-        res.send(data);
+        if (pageNumber) {
+            const startIndex = (pageNumber - 1) * recordLimit;
+            const pagedData = data.slice(startIndex, startIndex + recordLimit);
+            if (pagedData.length > 0) {
+                res.json({ status: 200, message: 'Paged Users fetched...', users: pagedData });
+            } else {
+                res.json({ status: 404, error: 'Paged Users not found...' });
+            }
+        } else {
+            res.json({ status: 200, message: 'All Users fetched...', users: data });
+        }
+    } catch (err) {
+        res.json({ status: 400, message: 'Error Fetching Paged Data...' });
     }
-})
+});
 
-
-
-
-// -------------------Using file system module -------------------------
-
-// app.get('/user', (req, res) => {
-//     const pageno = +req.query.pageno;
-//     fs.readFile('data.json', 'utf8', (err, fdata) => {
-//         const data = JSON.parse(fdata);
-//         try {
-//             if (pageno) {
-//                 const recordLimit = 10;
-//                 const start = (pageno - 1) * recordLimit;
-//                 const pagedData = data.slice(start, start + recordLimit);
-//                 res.send(pagedData);
-//             } else {
-//                 res.send(data);
-//             }
-//         }catch(err){
-//             res.send(err);
-//         }
-//     })
-// })
-
-// app.get('/user/:id', (req, res) => {
-//     const userID = req.params.id;
-//     fs.readFile('data.json', 'utf8', (err, fdata) => {
-//         if (err) console.log(err);
-//         try {
-//             const data = JSON.parse(fdata);
-//             const user = data.find(usr => String(usr.id) === userID);
-//             // console.log(user);
-//             if (user) {
-//                 res.send(user);
-//             } else {
-//                 res.send('Error Finding User...')
-//             }
-//         }catch(err){
-//             res.send(err);
-//         } 
-//     })
-//     // console.log(userID);
-//     // res.send(userID);
-
-//     // console.log(res.send('Error Finding User...'))
-// })
-
-
-
-
-app.listen(5000, () => console.log('Server Running on PORT - 5000... '))
+app.listen(process.env.PORT, () => console.log('Server Running on PORT', process.env.PORT));
